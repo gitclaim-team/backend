8dc83962610cfa6e5df3764e1ccc39ec5e56cc2d
Guil. Sperb Machado <guil@axlabs.com>
2025-05-31 16:10:35 +0200

    Enable CORS in NestJS application to allow requests from localhost and cha-ching.it domains with credentials support.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      /^(https?:\/\/)?localhost(:\d+)?$/,
+      /^https?:\/\/(.*\.)?cha-ching\.it(:\d+)?$/
+    ],
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
